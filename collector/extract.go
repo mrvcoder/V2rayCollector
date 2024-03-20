bea1dbe06645febe4b2cd2cc047eca220b34eeb5
MehraB832 <mehrabshafae@gmail.com>
2024-03-20 14:04:55 +0330

    add WaitGroup

--- collector/extract.go
+++ collector/extract.go
@@ -1,9 +1,11 @@
 package collector
 
 import (
-	"github.com/PuerkitoBio/goquery"
 	"regexp"
 	"strings"
+	"sync"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func ExtractConfig(Txt string, Tempconfigs []string) string {
@@ -37,6 +39,9 @@ func ExtractConfig(Txt string, Tempconfigs []string) string {
 	return d
 }
 
+var wg sync.WaitGroup
+var wgIn sync.WaitGroup
+
 func CrawlForV2ray(doc *goquery.Document, channelLink string, HasAllMessagesFlag bool) {
 	// here we are updating our DOM to include the x messages
 	// in our DOM and then extract the messages from that DOM
@@ -51,37 +56,52 @@ func CrawlForV2ray(doc *goquery.Document, channelLink string, HasAllMessagesFlag
 	// extract v2ray based on message type and store configs at [configs] map
 	if HasAllMessagesFlag {
 		// get all messages and check for v2ray configs
-		doc.Find(".tgme_widget_message_text").Each(func(j int, s *goquery.Selection) {
-			// For each item found, get the band and title
-			messageText, _ := s.Html()
-			str := strings.Replace(messageText, "<br/>", "\n", -1)
-			doc, _ := goquery.NewDocumentFromReader(strings.NewReader(str))
-			messageText = doc.Text()
-			line := strings.TrimSpace(messageText)
-			lines := strings.Split(line, "\n")
-			for _, data := range lines {
-				extractedConfigs := strings.Split(ExtractConfig(data, []string{}), "\n")
-				for _, extractedConfig := range extractedConfigs {
-					extractedConfig = strings.ReplaceAll(extractedConfig, " ", "")
-					if extractedConfig != "" {
+		wg.Add(1)
 
-						// check if it is vmess or not
-						re := regexp.MustCompile(Myregex["vmess"])
-						matches := re.FindStringSubmatch(extractedConfig)
+		go func() {
+			defer wg.Done()
 
-						if len(matches) > 0 {
-							extractedConfig = EditVmessPs(extractedConfig, "mixed", false)
-							if line != "" {
-								Configs["mixed"] += extractedConfig + "\n"
+			doc.Find(".tgme_widget_message_text").Each(func(j int, s *goquery.Selection) {
+				// For each item found, get the band and title
+				messageText, _ := s.Html()
+				str := strings.Replace(messageText, "<br/>", "\n", -1)
+				doc, _ := goquery.NewDocumentFromReader(strings.NewReader(str))
+				messageText = doc.Text()
+				line := strings.TrimSpace(messageText)
+				lines := strings.Split(line, "\n")
+				wgIn.Add(1)
+				go func() {
+					defer wgIn.Done()
+
+					for _, data := range lines {
+						extractedConfigs := strings.Split(ExtractConfig(data, []string{}), "\n")
+						for _, extractedConfig := range extractedConfigs {
+							extractedConfig = strings.ReplaceAll(extractedConfig, " ", "")
+							if extractedConfig != "" {
+
+								// check if it is vmess or not
+								re := regexp.MustCompile(Myregex["vmess"])
+								matches := re.FindStringSubmatch(extractedConfig)
+
+								if len(matches) > 0 {
+									extractedConfig = EditVmessPs(extractedConfig, "mixed", false)
+									if line != "" {
+										Configs["mixed"] += extractedConfig + "\n"
+									}
+								} else {
+									Configs["mixed"] += extractedConfig + "\n"
+								}
+
 							}
-						} else {
-							Configs["mixed"] += extractedConfig + "\n"
 						}
-
 					}
-				}
-			}
-		})
+				}()
+				wgIn.Wait()
+
+			})
+		}()
+
+		wg.Wait()
 	} else {
 		// get only messages that are inside code or pre tag and check for v2ray configs
 		doc.Find("code,pre").Each(func(j int, s *goquery.Selection) {
